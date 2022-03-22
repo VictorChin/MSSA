9ba5f570e19055614ec2cc28d53bb29fdbc29240
Victor Chin <victor@objectmage.com>
2022-03-22 11:24:23 -0400

    Added Form with BuiltIn Components

--- Week1/TodoList/TodoItem.cs
+++ Week1/TodoList/TodoItem.cs
@@ -2,4 +2,6 @@ public class TodoItem
 {
     public string? Title { get; set; }
     public bool IsDone { get; set; }
+    public string? Description {get;set;}
+    public System.DateTime? DueDate{get;set;}
 }
